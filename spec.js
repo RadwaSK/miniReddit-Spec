114a81a39c47f7f0942247ec7009609e3a0cf428
Abdelrahman_Badr <elnagmy45@gmail.com>
2019-03-23 21:29:35 +0200

    Badr-Voting and partially login

--- spec.js
+++ spec.js
@@ -6,6 +6,18 @@ describe('open Reddit', function() {
 });
 
 
+
+describe('Home', function() {
+  it('should return home after click on logo', function() {
+    browser.get('http://localhost:4200/');
+    element(by.id('logo')).click();
+    browser.sleep(3000);
+    expect(browser.getCurrentUrl()).toBe('http://localhost:4200/');
+  });
+});
+
+
+
 describe('Log In Reddit', function() {
   it('should open login window then enter valid username and password', function() {
     browser.get('http://localhost:4200/');
@@ -16,3 +28,54 @@ describe('Log In Reddit', function() {
     element(by.id('signin')).click();
   });
 });
+
+describe('Log In Reddit', function() {
+  it('should open login window then close', function() {
+    browser.get('http://localhost:4200/');
+    element(by.id('log-in')).click();
+    browser.sleep(3000);
+   element(by.className('close pull-right')).click();
+  });
+});
+
+
+describe('Log In Reddit', function() {
+  it('should open login window then close enter values then close and open again check for data', function() {
+    browser.get('http://localhost:4200/');
+    element(by.id('log-in')).click();
+    browser.sleep(3000);
+
+    var user=element(by.css('[formcontrolname="my_username"]'));
+    var pass=element(by.css('[formcontrolname="password"]'));
+
+    expect(user.getAttribute('value')).toEqual('');
+     expect(pass.getAttribute('value')).toEqual('');   
+  });
+
+});
+
+describe('Log In Reddit', function() {
+  it('should open login window then close enter values then close and open again check for data', function() {
+    browser.get('http://localhost:4200/');
+    element(by.id('log-in')).click();
+    browser.sleep(3000);
+
+     element(by.css('[formcontrolname="my_username"]')).sendKeys('Badr');
+    element(by.css('[formcontrolname="password"]')).sendKeys('1234');
+   element(by.className('close pull-right')).click();
+    element(by.id('log-in')).click();
+    browser.sleep(3000);
+
+    var user=element(by.css('[formcontrolname="my_username"]'));
+    var pass=element(by.css('[formcontrolname="password"]'));
+
+    expect(user.getAttribute('value')).toEqual('');
+     expect(pass.getAttribute('value')).toEqual('');   
+  });
+
+});
+
+
+
+
+
---
+++ vote.js
@@ -0,0 +1,85 @@
+describe('Voteing', function() {
+  it('should upvote', function() {
+    browser.get('http://localhost:4200/');
+     element(by.id('upVote')).click();
+     expect(element(by.id('voteNum')).getText()).toEqual('6');
+ });
+
+});
+
+
+describe('Voteing', function() {
+  it('should upvote 3 times', function() {
+    browser.get('http://localhost:4200/');
+     element(by.id('upVote')).click();
+     element(by.id('upVote')).click();
+     element(by.id('upVote')).click();
+     expect(element(by.id('voteNum')).getText()).toEqual('6');
+  
+ });
+
+});
+
+describe('Voteing', function() {
+  it('should downvoting', function() {
+    browser.get('http://localhost:4200/');
+     element(by.id('downVote')).click();
+     expect(element(by.id('voteNum')).getText()).toEqual('4');
+ });
+
+});
+
+describe('Voteing', function() {
+  it('should downvoting twice', function() {
+    browser.get('http://localhost:4200/');
+     element(by.id('downVote')).click();
+     element(by.id('downVote')).click();
+     expect(element(by.id('voteNum')).getText()).toEqual('5');
+  
+ });
+
+});
+
+describe('Voteing', function() {
+  it('should upvote and downvote second post', function() {
+    browser.get('http://localhost:4200/');
+    element(by.id('downVote1')).click();
+    element(by.id('upVote1')).click();
+    expect(element(by.id('voteNum1')).getText()).toEqual('13');
+ });
+
+});
+
+
+
+
+describe('Voteing', function() {
+  it('should upvote and downvote first post', function() {
+    browser.get('http://localhost:4200/');
+    element(by.id('downVote')).click();
+    element(by.id('upVote')).click();
+    expect(element(by.id('voteNum')).getText()).toEqual('5');
+  
+ });
+
+});
+
+
+
+
+describe('Voteing', function() {
+  it('should upvote and downvote for more than post', function() {
+    browser.get('http://localhost:4200/');
+   
+    element(by.id('upVote2')).click();
+    element(by.id('downVote1')).click();
+    element(by.id('downVote')).click();
+
+
+    expect(element(by.id('voteNum')).getText()).toEqual('4');
+    expect(element(by.id('voteNum1')).getText()).toEqual('12');
+    expect(element(by.id('voteNum2')).getText()).toEqual('6');
+  
+ });
+
+});
