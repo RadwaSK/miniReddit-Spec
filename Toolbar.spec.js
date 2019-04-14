1a564cf7042b74295ba00eb6382ef49d5dddd8dc
RadwaSK <radwasm@hotmail.com>
2019-04-14 06:32:09 +0200

    Update Toolbar.spec.js

--- Toolbar.spec.js
+++ Toolbar.spec.js
@@ -13,4 +13,10 @@ describe('Toolbar', () => {
 		element(by.id('sign-up')).click();
 		browser.wait(EC.presenceOf(element(by.buttonText('Next'))), 1000);
 	});
+
+	it('should have sign up button that opens a pop-up', ()=>{
+		element(by.id('right-dropdown')).click();
+		browser.wait(EC.presenceOf(element(by.id('dropdown-menu-right'))), 1000);
+	});
+	
 });
