f6e61c770482883113e9c663be760414a6240616
Abdelrahman_Badr <elnagmy45@gmail.com>
2019-05-06 00:45:58 +0200

    Final Push Badr

--- Toolbar.spec.js
+++ Toolbar.spec.js
@@ -12,21 +12,33 @@ describe('Toolbar', () => {
 	it('should have sign up button that opens a pop-up', ()=>{
 		element(by.id('sign-up')).click();
 		browser.wait(EC.presenceOf(element(by.buttonText('Next'))), 1000);
+    browser.sleep(5000);
 	});
 	 var EC = protractor.ExpectedConditions;
-    it('check loged in ', function() {
+   it('check loged in ', function() {
+      it('check loged in ', function() {
      browser.get('http://35.204.169.121/Reddit/');
      browser.wait(EC.urlIs('http://35.204.169.121/Reddit/'), 5000); 
      browser.sleep(5000);
-    browser.wait(EC.presenceOf(element(by.id('onesignal-popover-container')).element(by.id('onesignal-popover-cancel-button'))), 5000);
-    element(by.id('onesignal-popover-container')).element(by.id('onesignal-popover-cancel-button')).isPresent().then(function(result) {
+    browser.wait(EC.presenceOf(element(by.buttonText('No Thanks')), 5000));
+    element(by.buttonText('No Thanks')).isPresent().then(function(result) {
     if ( result ) {
-         element(by.id('onesignal-popover-container')).element(by.id('onesignal-popover-cancel-button')).click();
+         element(by.buttonText('No Thanks')).click();
     } else {
         //do nothing
     }
 
    });
+    browser.sleep(5000);
+    element(by.id('log-in')).click();
+    browser.sleep(3000);
+    var user='mini-reddit'; var pass='mini-reddit';
+    element(by.css('[formcontrolname="username"]')).sendKeys(user);
+    element(by.css('[formcontrolname="password"]')).sendKeys(pass);
+    element(by.id('signin')).click();
+    browser.sleep(3000);
+    expect(element(by.id('UserName')).getText()).toEqual(user);
+  });
    
   
     browser.sleep(5000);
@@ -46,4 +58,11 @@ describe('Toolbar', () => {
 		 browser.sleep(3000);
 		expect(browser.getCurrentUrl()).toBe('http://35.204.169.121/Reddit/settings'); 
 	});
+    it('should log out', function() {
+
+    element(by.id('right-dropdown')).click();
+    element(by.linkText('Log Out')).click();
+    browser.sleep(3000);
+    expect(element(by.id('log-in')).getText()).toEqual('Log in');
+  });
 });
--- vot.js
+++ vot.js
@@ -1,31 +1,7 @@
 describe('Profile', function() {
-    var EC = protractor.ExpectedConditions;
-    function toNumber(promiseOrValue) {
-    // if it is not a promise, then convert a value
-        if (!protractor.promise.isPromise(promiseOrValue)) {
-            return parseInt(promiseOrValue, 10);
-        }
-    // if promise - convert result to number
-        return promiseOrValue.then(function (stringNumber) {
-            return parseInt(stringNumber, 10);
-        });
-    }
+   
 
-it('check loged in ', function() {
-   browser.get('http://35.204.169.121/Reddit/');
-   browser.wait(EC.urlIs('http://35.204.169.121/Reddit/'), 5000); 
-   browser.sleep(5000);
-   element(by.id('onesignal-popover-cancel-button')).click();
-    browser.sleep(2000);
-   element(by.id('log-in')).click();
-   browser.sleep(3000);
-   var user='menna'; var pass='123456789';
-   element(by.css('[formcontrolname="username"]')).sendKeys(user);
-   element(by.css('[formcontrolname="password"]')).sendKeys(pass);
-   element(by.id('signin')).click();
-   browser.sleep(3000);
-   expect(element(by.id('UserName')).getText()).toEqual(user);
-});
+
 
 
 it('should upvote 3 times', function() {
