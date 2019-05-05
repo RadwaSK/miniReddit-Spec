9344bb96c31b3c71328e299087e811c8679f1e87
RadwaSK <radwasm@hotmail.com>
2019-05-05 03:41:07 +0200

    Update stressTests.py

--- stressTests.py
+++ stressTests.py
@@ -28,7 +28,7 @@ def signUp(l):
 	email = user + '@email.com'
 	fakeUser = {"username": user, "password": password, "password_confirmation": password, "email": email}
 	l.users.append({"username": user, "password": password})
-	response = l.client.post("api/unauth/signUp",  headers=headers1, json=fakeUser)
+	response = l.client.post("api/v1/unauth/signUp",  headers=headers1, json=fakeUser)
 	dataJSON = response.json()
 	l.token = dataJSON['token']
 
@@ -38,14 +38,14 @@ def deleteMyAccount(l):
 		i = randint(0, len(l.users)-1)
 	user = l.users[i]
 	password = user['password']
-	l.client.post("api/auth/deleteMyAccount",
+	l.client.post("api/v1/auth/deleteMyAccount",
     	headers=headers3(l.token),
     	json={"password": password})
 
 #on_start
 def signIn(l):
 	i = randint(0, len(l.users)-1)
-	response = l.client.post('api/unauth/signIn', 
+	response = l.client.post('api/v1/unauth/signIn', 
 		json=l.users[i],
 		headers=headers1)
 	dataJSON = response.json()
@@ -53,7 +53,7 @@ def signIn(l):
 
 #on_stop
 def signOut(l):
-	l.client.post('api/auth/signOut',
+	l.client.post('api/v1/auth/signOut',
 		headers=headers2(l.token))
 
 ########################################################################################################################################
@@ -62,7 +62,7 @@ def createCommunity(l):
 	commName = randstr(8)
 	commID = max(l.communityIDs)+1
 	l.communityIDs.append(commID)
-	l.client.post('api/auth/createCommunity',
+	l.client.post('api/v1/auth/createCommunity',
 		headers=headers3(l.token),
 		json={'community_name': commName})
 
@@ -70,21 +70,21 @@ def editCommunity(l):
 	commID = l.communityIDs[randint(0, len(l.communityIDs)-1)]
 	randomData = {'community_id': commID, 'rules_content': randstr(10),
 		'des_content': randstr(10), 'banner': randstr(5), 'logo': randstr(5)}
-	l.client.post('api/auth/editCommunity',
+	l.client.post('api/v1/auth/editCommunity',
 		headers=headers3(l.token),
 		json=randomData)
 
 def removeCommunity(l):
 	commID = l.communityIDs[randint(0, len(l.communityIDs)-1)]
 	l.communityIDs.remove(commID)
-	l.client.post('api/auth/removeCommunity',
+	l.client.post('api/v1/auth/removeCommunity',
 		headers=headers3(l.token),
 		json={"community_id": commID})
 
 def addModerator(l):
 	commID = l.communityIDs[randint(0, len(l.communityIDs)-1)]
 	modName = randstr(8)
-	l.client.post('api/auth/addModerator',
+	l.client.post('api/v1/auth/addModerator',
 		headers=headers3(l.token),
 		json={'community_id': commID, 'moderator_username':modName})
 
@@ -93,25 +93,25 @@ def removeModerator(l):
 	i = randint(0, len(l.users)-1)
 	moderator = users[i]
 	modName = moderator['username']
-	l.client.post('api/auth/removeModerator',
+	l.client.post('api/v1/auth/removeModerator',
 		headers=headers3(l.token),
 		json={'community_id':commID, 'moderator_username':modName})
 
 def viewModerators(l):
 	commID = l.communityIDs[randint(0, len(l.communityIDs)-1)]
-	l.client.get('api/unauth/viewModerators',
+	l.client.get('api/v1/unauth/viewModerators',
 		headers=headers1,
 		json={'community_id':commID})
 
 def subscribeCommunity(l):
 	commID = l.communityIDs[randint(0, len(l.communityIDs)-1)]
-	l.client.post('api/auth/subscribeCommunity',
+	l.client.post('api/v1/auth/subscribeCommunity',
 		headers=headers3(l.token),
 		json={'community_id': commID})
 
 def unSubscribeCommunity(l):
 	commID = l.communityIDs[randint(0, len(l.communityIDs)-1)]
-	l.client.post('api/auth/unSubscribeCommunity',
+	l.client.post('api/v1/auth/unSubscribeCommunity',
 		headers=headers3(l.token),
 		json={'community_id': commID})
 
@@ -119,13 +119,13 @@ def viewUserCommunities(l):
 	i = randint(0, len(l.users)-1)
 	user = users[i]
 	name = moderator['username']
-	l.client.get('api/unauth/viewUserCommunities',
+	l.client.get('api/v1/unauth/viewUserCommunities',
 		headers=headers1,
 		json={'username': name})
 
 def communityInformation(l):
 	commID = l.communityIDs[randint(0, len(l.communityIDs)-1)]
-	l.client.get('api/unauth/communityInformation',
+	l.client.get('api/v1/unauth/communityInformation',
 		headers=headers1,
 		json={'community_id': commID})
 
@@ -137,7 +137,7 @@ def saveLink(l):
 	
 	l.savedLinks.append(linkID)
 
-	l.client.post('api/auth/saveLink',
+	l.client.post('api/v1/auth/saveLink',
 		headers=headers3(l.token),
 		json={'link_id': link})
 
@@ -147,7 +147,7 @@ def unSaveLink(l):
 	
 	l.savedLinks.remove(linkID)
 	
-	l.client.post('api/auth/unsaveLink',
+	l.client.post('api/v1/auth/unsaveLink',
 		headers=headers3(l.token),
 		json={'link_id': linkID})
 
@@ -159,7 +159,7 @@ def addLink(l):
 		postTitle = randstr(10)
 		postID = max(l.postsID)+1
 		l.postsID.append(postID)
-		l.client.post('api/auth/addLink',
+		l.client.post('api/v1/auth/addLink',
 			headers=headers3(l.token),
 			json={'post_content':postContent, 'post_title': postTitle});
 
@@ -167,7 +167,7 @@ def addLink(l):
 		commentContent = randstr(10)
 		parentID = l.postsID[randint(0, len(l.postsID)-1)]
 		l.commentsID.append(max(l.commentsID)+1)
-		l.client.post('api/auth/addLink',
+		l.client.post('api/v1/auth/addLink',
 			headers=headers3(l.token),
 			json={'post_content':commentContent, 'parent_link_id': parentID});
 
@@ -180,7 +180,7 @@ def pinPost(l):
 	else:
 		l.pinnedPosts.append(postID)
 
-	l.client.patch('api/auth/pinPost',
+	l.client.patch('api/v1/auth/pinPost',
 		headers=headers3(l.token),
 		json={'post_id': postID})
 
@@ -188,7 +188,7 @@ def removeLink(l):
 	i = randint(0, len(l.postsID)-1)
 	linkID = l.postsID[i]
 	l.postsID.remove(linkID)
-	l.client.post('api/auth/removeLink',
+	l.client.post('api/v1/auth/removeLink',
 		headers=headers3(l.token),
 		json={'link_id': linkID})
 	
@@ -197,7 +197,7 @@ def hidePost(l):
 	i = randint(0, len(l.postsID)-1)
 	linkID = l.postsID[i]
 	l.hiddenPosts.append(linkID)
-	l.client.post('api/auth/hidePost',
+	l.client.post('api/v1/auth/hidePost',
 		headers=headers3(l.token),
 		json={'post_id': linkID})
 
@@ -205,7 +205,7 @@ def unhidePost(l):
 	i = randint(0, len(l.hiddenPosts)-1)
 	linkID = l.hiddenPosts[i]
 	l.hiddenPosts.remove(linkID)
-	l.client.post('api/auth/unhidePost',
+	l.client.post('api/v1/auth/unhidePost',
 		headers=headers3(l.token),
 		json={'post_id': linkID})
 
@@ -214,7 +214,7 @@ def editPost(l):
 	i = randint(0, len(l.postsID)-1)
 	linkID = l.postsID[i]
 	fakePost = {"post_id": linkID}
-	l.client.patch('api/auth/editPost',
+	l.client.patch('api/v1/auth/editPost',
 		headers=headers3(l.token),
 		json=fakePost)
 
@@ -223,57 +223,57 @@ def editComment(l):
 	i = randint(0, len(l.commentsID)-1)
 	commentID = l.commentsID[i]
 	randContent = randstr(10)
-	l.client.patch('api/auth/editComment',
+	l.client.patch('api/v1/auth/editComment',
 		headers=headers3(l.token),
 		json={'comment_id': commentID, 'new_content': randContent})
 
 def upvoteLink(l):
 	i = randint(0, len(l.postsID)-1)
 	linkID = l.postsID[i]
-	l.client.post('api/auth/upvoteLink',
+	l.client.post('api/v1/auth/upvoteLink',
 		headers=headers3(l.token),
 		json={'link_id': linkID})
 
 def downvoteLink(l):
 	i = randint(0, len(l.postsID)-1)
 	linkID = l.postsID[i]
-	l.client.post('api/auth/downvoteLink',
+	l.client.post('api/v1/auth/downvoteLink',
 		headers=headers3(l.token),
 		json={'link_id': linkID})
 
 def viewUpOrDownvotedPosts(l):
 	boolean = randint(0,1)
-	l.client.get('api/auth/viewUpOrDownvotedPosts',
+	l.client.get('api/v1/auth/viewUpOrDownvotedPosts',
 		headers=headers3(l.token),
 		json={'type': boolean})
 
 def viewHiddenPosts(l):
-	l.client.get('api/auth/viewHiddenPosts',
+	l.client.get('api/v1/auth/viewHiddenPosts',
 		headers=headers2(l.token))
 
 def viewOverview(l):
 	i = randint(0, len(l.users)-1)
 	user = l.users[i]
 	name = user['username']
-	l.client.get('api/auth/viewOverview',
+	l.client.get('api/v1/auth/viewOverview',
 	headers=headers3(l.token),
 	json={'username': name})
 
 def viewSavedLinks(l):
-	l.client.get('api/auth/viewSavedLinks',
+	l.client.get('api/v1/auth/viewSavedLinks',
 		headers=headers2(l.token))
 
 def giveReward(l):
 	i = randint(0, len(l.users)-1)
 	user = l.users[i]
 	name = user['username']
-	l.client.post('api/auth/giveReward',
+	l.client.post('api/v1/auth/giveReward',
 		headers=headers3(l.token),
 		json={'username': name})
 
 def uploadImage(l):
 	randImage = randstr(5)
-	l.client.post('api/auth/uploadImage',
+	l.client.post('api/v1/auth/uploadImage',
 		headers=headers3(l.token),
 		json={'uploaded_image': 'a file'})
 
@@ -286,7 +286,7 @@ def ViewPosts(l):
 	name = user['username']
 	i = randint(0, len(l.communityIDs)-1)
 	commID = l.communityIDs[i]
-	l.client.get('api/unauth/ViewPosts',
+	l.client.get('api/v1/unauth/ViewPosts',
 		headers=headers1,
 		json={'page_type': pageType, 'username': name, 'community_id': commID})
 
@@ -294,7 +294,7 @@ def viewComments(l):
 	i = randint(0, len(l.users)-1)
 	user = l.users[i]
 	name = user['username']
-	l.client.get('api/unauth/viewComments',
+	l.client.get('api/v1/unauth/viewComments',
 		headers=headers1,
 		json={'username': name})
 
@@ -302,39 +302,39 @@ def viewComments(l):
 def viewCommentsReplies(l):
 	i = randint(0, len(l.postsID)-1)
 	linkID = l.postsID[i]
-	l.client.get('api/unauth/viewCommentsReplies',
+	l.client.get('api/v1/unauth/viewCommentsReplies',
 		headers=headers1,
 		json={'link_id': linkID})
 
 def viewSinglePost(l):
 	i = randint(0, len(l.postsID)-1)
 	linkID = l.postsID[i]
-	l.client.get('api/unauth/viewSinglePost',
+	l.client.get('api/v1/unauth/viewSinglePost',
 	headers=headers1,
 	json={'post_id': linkID})
 
 def updateDisplayName(l):
 	name = randstr(7)
-	l.client.patch("api/auth/updateDisplayName",
+	l.client.patch("api/v1/auth/updateDisplayName",
     headers=headers3(l.token),
     json={"name":name})
 
 def updateAbout(l):
 	about = randstr(10)
-	l.client.patch("api/auth/pdateAbout",
+	l.client.patch("api/v1/auth/pdateAbout",
     	headers=headers3(l.token),
     	json={"about":"kvQwJ1LWb29MsKrE"})
 
 def updateCoverAndProfileImage(l):
 	image = randstr(5)
 	i = randint(1, 2)
-	l.client.post("api/auth/updateCoverAndProfileImage",
+	l.client.post("api/v1/auth/updateCoverAndProfileImage",
     headers=headers3(l.token),
     json={"profile_image":"image", "profile_or_cover":i})
 
 def forgetPassword(l):
 	email = randstr(5) + '@email.com'
-	l.client.post("api/unauth/forgetPassword",
+	l.client.post("api/v1/unauth/forgetPassword",
     headers=headers1,
     json={"email":email})
 
@@ -343,7 +343,7 @@ def changePass(l):
 	oldPass = users[i]['password']
 	newPass = randstr(8)
 	users[i]['password'] = newPass
-	l.client.patch("api/auth/change/password",
+	l.client.patch("api/v1/auth/change/password",
     	headers=headers3(l.token),
     	json={"password":oldPass, "new_password": newPass, "confirm_new_password":newPass})
 
@@ -356,24 +356,24 @@ def sendMessage(l):
 	i = randint(0, len(l.users)-1)
 	recUSer = l.users[i]['username']
 	randContent = randstr(10)
-	l.client.post("api/auth/sendMessage",
+	l.client.post("api/v1/auth/sendMessage",
 	headers=headers3(l.token),
 	json={"rec_username":recUser, "msg_content": randContent})
 
 def viewUserMessages(l):
 	i = randint(0, len(l.messages)-1)
 	msgID = l.messages[i]
-	l.client.get("api/auth/viewUserMessage",
+	l.client.get("api/v1/auth/viewUserMessage",
 	headers=headers3(l.token),
 	json={"message_id": msgID})
 
 def viewUserSentMessages(l):
-	l.client.get("api/auth/viewUserSentMessages",
+	l.client.get("api/v1/auth/viewUserSentMessages",
 		headers=headers2(l.token))
 
 def viewUserInboxMessages(l):
 	i = randint(1, 3)
-	l.client.get("api/auth/viewUserInboxMessages",
+	l.client.get("api/v1/auth/viewUserInboxMessages",
 	headers=headers3(l.token),
 	json={"state": i})
 
@@ -384,7 +384,7 @@ def followers(l):
 	i = randint(0, len(l.users)-1)
 	user = l.users[i]
 	name = user['username']
-	l.client.get("api/auth/followers",
+	l.client.get("api/v1/auth/followers",
 		headers=headers3(l.token),
 		json={"username":name})
 
@@ -392,7 +392,7 @@ def following(l):
 	i = randint(0, len(l.users)-1)
 	user = l.users[i]
 	name = user['username']
-	l.client.get("api/auth/following",
+	l.client.get("api/v1/auth/following",
 		headers=headers3(l.token),
 		json={"username":name})
 
@@ -401,7 +401,7 @@ def follow(l):
 	user = l.users[i]
 	name = user['username']
 	l.followedUsersList.append(name)
-	l.client.post("api/auth/follow",
+	l.client.post("api/v1/auth/follow",
 		headers=headers3(l.token),
 		json={"username":name})
 
@@ -409,7 +409,7 @@ def unfollow(l):
 	i = randint(0, len(l.followedUsersList)-1)
 	name = l.followedUsersList[i]
 	l.followedUsersList.remove(name)
-	l.client.post("api/auth/unfollow",
+	l.client.post("api/v1/auth/unfollow",
 		headers=headers3(l.token),
 		json={"username":name})
 
@@ -417,15 +417,15 @@ def unfollow(l):
 #notifications requests
 
 def checkNotification(l):
-	l.client.get("api/auth/checkNotification",
+	l.client.get("api/v1/auth/checkNotification",
 		headers=headers2(l.token))
 
 def pushNotification(l):
-	l.client.get("api/auth/pushNotification",
+	l.client.get("api/v1/auth/pushNotification",
 		headers=headers2(l.token))
 
 def blockedUsers(l):
-	l.client.get("api/auth/blockedUsers",
+	l.client.get("api/v1/auth/blockedUsers",
 		headers=headers2(l.token))
 
 def blockUser(l):
@@ -433,7 +433,7 @@ def blockUser(l):
 	user = l.users[i]
 	name = user['username']
 	l.blockedUsersList.append(name)
-	l.client.post("api/auth/blockUser",
+	l.client.post("api/v1/auth/blockUser",
 		headers=headers3(l.token),
 		json={"username": name})
 
@@ -441,29 +441,29 @@ def unblockUser(l):
 	i = randint(0, len(l.blockedUsersList)-1)
 	name = l.blockedUsersList[i]
 	l.blockedUsersList.remove(name)
-	l.client.post("api/auth/unblockUser",
+	l.client.post("api/v1/auth/unblockUser",
 		headers=headers3(l.token),
 		json={"username": name})
 
 def search(l):
 	randContent = randstr(10)
-	l.client.get("api/unauth/search",
+	l.client.get("api/v1/unauth/search",
 		headers=headers1,
 		json={"search_content":randContent})
 
 def viewPrivateUserInfo(l):
-	l.client.get("api/auth/viewPrivateUserInfo",
+	l.client.get("api/v1/auth/viewPrivateUserInfo",
 		headers=headers2(l.token))
 
 def getUsername(l):
-	l.client.get("api/auth/getUsername",
+	l.client.get("api/v1/auth/getUsername",
 		headers=headers2(l.token))
 
 def viewPublicUserInfo(l):
 	i = randint(0, len(l.users)-1)
 	user = l.users[i]
 	name = user['username']
-	l.client.get("api/unauth/viewPublicUserInfo",
+	l.client.get("api/v1/unauth/viewPublicUserInfo",
 		headers=headers1,
 		json={"username":name})
 
